1b37cb98548b90e8e09912100eb9ae745606dea1
IDontCodee <94655562+IDontCodee@users.noreply.github.com>
2022-06-29 20:38:55 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/ultraviolet/',
-    bare: 'https://deployable-bare-server.herokuapp.com/bare/',
+    bare: 'https://inco.tk/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
